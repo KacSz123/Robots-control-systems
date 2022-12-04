d82fd4551bcaffd1984ff466bbaa58233af946d6
Kacper Szczepanowski <kacperszcz159@gmail.com>
2022-12-04 12:46:25 +0100

    model of the  RTR manipulator

--- RTR.m
+++ RTR.m
@@ -6,7 +6,11 @@ m2 = 20;        %[kg]
 m3 = 20;        %[kg]
 g = 9.81;       %[m/(s^2)]
 %% init simulink
-[t] = sim("TRT_sim.slx");
+Kp = 100000;
+Kd = 10000;
+[t] = sim("RTR_sim2021a.slx");
 
+if 1==1
+end
 
 
