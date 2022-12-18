b3214f677972c77a0c54d0b711303fa38a0f6452
Kacper Szczepanowski <kacperszcz159@gmail.com>
2022-12-18 21:34:27 +0100

    first work with Sadegh and Horowitz algorithm

--- RTR.m
+++ RTR.m
@@ -6,7 +6,7 @@ m2 = 20;        %[kg]
 m3 = 20;        %[kg]
 g = 9.81;       %[m/(s^2)]
 %% sim
-t_stop = 5;
+t_stop = 10;
 %% regulator PD
 Kp = 2000;
 Kd = 200;
@@ -19,11 +19,13 @@ q1p_0 = 0;
 q2p_0 = 0;
 q3p_0 = 0;
 %% init conditions trajectory generator
-q1d_0 = 0; % q1d = 2sin(t/2)
-q2d_0 = 2; % q2d = 2cos(t)
+q1d_0 = 0; % q1d = sin(t/2)
+q2d_0 = 1; % q2d = cos(t)
 q3d_0 = 0; % q3d = sin(2t)
 
-
+q1dp_0 = 0.5;
+q2dp_0 = 0;
+q3dp_0 = 2;
 
 %% simulink model
 [t] = sim("RTR_sim2021a.slx");
@@ -37,7 +39,7 @@ figure(2); grid on;
 plot(t.t, t.e2,'r', linewidth=1.5);
 xlabel('$t$', 'interpreter', 'latex',fontsize=14);
 ylabel('$e_2(t)$', 'interpreter', 'latex',fontsize=14);
-title('Zmiana $e_2$ w czasie',fontsize=16);
+title('Zmiana $e_2$ w czasie', 'interpreter', 'latex',fontsize=16);
 figure(3); grid on;
 plot(t.t, t.e3,'r', linewidth=1.5);
 xlabel('$t$', 'interpreter', 'latex',fontsize=14);
