c8a4a4281d4673a74f583228c56cace97393b6cb
Kacper Szczepanowski <kacperszcz159@gmail.com>
2023-06-13 20:51:58 +0200

    realese ver 0.9

--- RTR.m
+++ RTR.m
@@ -6,10 +6,10 @@ m2 = 20;        %[kg]
 m3 = 20;        %[kg]
 g = 9.81;       %[m/(s^2)]
 %% sim
-t_stop = 10;
+t_stop = 20;
 %% regulator PD
-Kp = 2000;
-Kd = 200;
+Kp = 200;
+Kd = 20;
 %% start pose
 q1_0 = 2;
 q2_0 = 4;
@@ -26,25 +26,40 @@ q3d_0 = 0; % q3d = sin(2t)
 q1dp_0 = 0.5;
 q2dp_0 = 0;
 q3dp_0 = 2;
-
+%% controller constants
+% Kp= 100;
+Kd = 10;  %% adap 75 kd
+Delta = 2;
+R = 20;
+Gamma = 20;
 %% simulink model
-[t] = sim("RTR_sim2021a.slx");
-%% plot e(t)
-figure(1); grid on; 
+[t] = sim("RTR_sim2021a_AdapSadHor.slx");
+% plot e(t)
+figure(1); grid on; hold on;
 plot(t.t, t.e1,'r', linewidth=1.5);
 xlabel('$t$', 'interpreter', 'latex',fontsize=14);
 ylabel('$e_1(t)$', 'interpreter', 'latex',fontsize=14);
 title('Zmiana $e_1$ w czasie', 'interpreter', 'latex', fontsize=16);
-figure(2); grid on;
+figure(2); grid on; hold on;
 plot(t.t, t.e2,'r', linewidth=1.5);
 xlabel('$t$', 'interpreter', 'latex',fontsize=14);
 ylabel('$e_2(t)$', 'interpreter', 'latex',fontsize=14);
 title('Zmiana $e_2$ w czasie', 'interpreter', 'latex',fontsize=16);
-figure(3); grid on;
+figure(3); grid on; hold on;
 plot(t.t, t.e3,'r', linewidth=1.5);
 xlabel('$t$', 'interpreter', 'latex',fontsize=14);
-ylabel('$e_3(t)$',fontsize=14);
+ylabel('$e_3(t)$', 'interpreter', 'latex',fontsize=14);
 title('Zmiana $e_3$ w czasie', 'interpreter', 'latex',fontsize=16);
+figure(4); grid on; hold on;
+plot(t.t, t.J,'r', linewidth=1.5);
+xlabel('$t$', 'interpreter', 'latex',fontsize=14);
+ylabel('$J(t)$', 'interpreter', 'latex',fontsize=14);
+title('Średni błąd kwadratowy',fontsize=16);
+figure(5); grid on; hold on;
+plot(t.t, t.a,'r', linewidth=1.5);
+xlabel('$t$', 'interpreter', 'latex',fontsize=14);
+ylabel('$a(t)$', 'interpreter', 'latex',fontsize=14);
+title('Estymowany parametr $\hat{a}$ ', 'interpreter', 'latex',fontsize=16);
 
 %% plot ep(t)
 % figure(4); grid on; 
